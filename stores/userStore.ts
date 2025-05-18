179fab58eea10d44784f944f4cb7a44089db99ef
Alexis Chaignon <alexis.chaignon@gmail.com>
2025-05-18 14:23:40 +0200

    feat: add token to user front

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -4,30 +4,37 @@ export const useUserStore = defineStore('userStore', {
   state: () => ({
     user: null as any | null,
     isAuthenticated: false as boolean,
+    token: null as string | null,
   }),
 
   actions: {
     setUser(user: any) {
       this.user = user
       this.isAuthenticated = true
+      this.token = user.token
 
       if (typeof window !== 'undefined') {
         localStorage.setItem('user', JSON.stringify(user))
+        localStorage.setItem('token', user.token)
       }
     },
     clearUser() {
       this.user = null
       this.isAuthenticated = false
+      this.token = null
 
       if (typeof window !== 'undefined') {
         localStorage.removeItem('user')
+        localStorage.removeItem('token')
       }
     },
     loadUserFromStorage() {
       if (typeof window !== 'undefined') {
         const userData = localStorage.getItem('user')
-        if (userData) {
+        const tokenData = localStorage.getItem('token')
+        if (userData && tokenData) {
           this.user = JSON.parse(userData)
+          this.token = tokenData
           this.isAuthenticated = true
         }
       }
@@ -37,5 +44,6 @@ export const useUserStore = defineStore('userStore', {
   getters: {
     getUser: (state) => state.user,
     isLoggedIn: (state) => state.isAuthenticated,
+    getToken: (state) => state.token
   }
 })
